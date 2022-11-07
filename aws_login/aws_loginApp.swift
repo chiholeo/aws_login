e1a77d4f49ad8d6343fb11a0c00434482a74745d
Harry Tse <ttc794@ha.org.hk>
2022-11-07 16:15:51 +0800

    Initial Commit

---
+++ aws_login/aws_loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  aws_loginApp.swift
+//  aws_login
+//
+//  Created by wmc560 on 7/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct aws_loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
